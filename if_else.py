95d4df69a4278bf9b90c764187777037453c4167
LoopBreaks <113218220+LoopBreaks@users.noreply.github.com>
2022-09-14 15:54:04 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,45 @@
+# a = 10
+# b = 10
+# if a<b:
+#     print("Body Of IF")
+
+# print(a<b)
+
+# if a>b:
+#     print("BOdy of If")
+
+
+# if False:
+#     print("BOdy of If")
+
+# if a>b:
+#     print("A is greater than B")
+# else:
+#     print("B is greater than a")
+#     print("Body Of Else")
+
+
+
+# Elif satatement
+# a = 5
+# b = 10
+
+# if a>=b:
+#     print("BOdy of If")
+#     print("A is greater than B")
+# elif b>=a:
+#     print("B is greater than A")
+#     print("BOdy of elif")
+# else:
+#     print("Value is not defined")
+
+
+a = 5
+b = 10
+
+if a!=b:
+    print("a is not qual to b")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("Value is unexpected")
